1fbe00f48e06722e5e2dd676d2fdfc251689ddc6
Melrain <yezyd92@icloud.com>
2023-09-28 13:08:41 +0800

    implement base tailwind and style guide

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,7 @@ export default function Home() {
   return (
     <div>
       <h1 className="h1-bold">Next.js13! We are coming!</h1>
+      <h2 className="h1-bold">Next.js13! We are coming!</h2>
     </div>
   );
 }
