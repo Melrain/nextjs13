b0965cfc507ceb634380aed38ba0e5d3a486e39e
Melrain <yezyd92@icloud.com>
2023-10-08 11:05:12 +0800

    019 navbar

---
+++ app/(root)/(home)/page.tsx
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <div>
+      <h1 className="flex-center h1-bold">HomePage</h1>
+      <h1 className="h2-bold">Next.js13! We are coming!</h1>
+      <h2 className="h3-bold">Next.js13! We are coming!</h2>
+    </div>
+  );
+}
--- app/(shopping)/cart/page.tsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-const cart = () => {
-  return <div className="flex-center">This is Shopping Cart page</div>;
-};
-
-export default cart;
