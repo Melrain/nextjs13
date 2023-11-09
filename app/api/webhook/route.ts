60fdffbef802e27181161480155c566ab2c2ade0
Melrain <yezyd92@icloud.com>
2023-11-09 14:22:59 +0800

    delete console log

--- app/api/webhook/route.ts
+++ app/api/webhook/route.ts
@@ -3,7 +3,6 @@ import { headers } from 'next/headers';
 import { WebhookEvent } from '@clerk/nextjs/server';
 import { createUser, deleteUser, updateUser } from '@/lib/actions/user.action';
 import { NextResponse } from 'next/server';
-import path from 'path';
 import console from 'console';
 
 export async function POST(req: Request) {
@@ -84,6 +83,7 @@ export async function POST(req: Request) {
       // 用户的个人主页
       path: `/profile/${id}`
     });
+    console.log(mongoUser);
   }
 
   // TODO - delete user in your database
